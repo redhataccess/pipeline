451350daabee57bb8422f90798d73ae1b31f459e
Flossy <sfloess@redhat.com>
2020-07-23 15:01:44 -0400

    Resolves #3 - now added a convenience method to Step that allows processing of a pipeline context but uses the internal step context

--- src/main/java/com/redhat/step/Step.java
+++ src/main/java/com/redhat/step/Step.java
@@ -1,6 +1,8 @@
 package com.redhat.step;
 
 import com.redhat.common.processor.Processor;
+import com.redhat.pipeline.PipelineContext;
+import java.util.Objects;
 
 /**
  * Interface for defining a pipeline step. Please note a step may be a pipeline step or a pipeline itself.
@@ -8,4 +10,13 @@ import com.redhat.common.processor.Processor;
  * @author sfloess
  */
 public interface Step extends Processor<StepContext> {
+    /**
+     * Requests the step to process a pipeline context. However, we will only process the step context
+     * found inside the pipeline context.
+     */
+    default public PipelineContext process(PipelineContext context) {
+        process(Objects.requireNonNull(context, "Must provide a non-null pipeline context!").getStepContext());
+
+        return context;
+    }
 }
